29ad20252084ff1c0d78ad5990e7d29f94d6f3a1
DESKTOP-V3K83C5\GuideJunG <guidellka@hotmail.com>
2017-03-22 20:07:18 +0700

    add design -> addPlace,Place,PlaceList

--- Project/src/GUI/PlaceList.java
+++ Project/src/GUI/PlaceList.java
@@ -12,7 +12,7 @@ package GUI;
 public class PlaceList extends javax.swing.JFrame {
 
     /**
-     * Creates new form userPlace
+     * Creates new form Place
      */
     public PlaceList() {
         initComponents();
@@ -27,17 +27,100 @@ public class PlaceList extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jScrollPane1 = new javax.swing.JScrollPane();
+        jTable1 = new javax.swing.JTable();
+        loginAs = new javax.swing.JLabel();
+        username = new javax.swing.JLabel();
+        placeNo = new javax.swing.JLabel();
+        number = new javax.swing.JLabel();
+        dateText = new javax.swing.JLabel();
+        date = new javax.swing.JLabel();
+        back = new javax.swing.JToggleButton();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setTitle("List Place");
+        setPreferredSize(new java.awt.Dimension(800, 600));
+        setResizable(false);
+
+        jTable1.setFont(new java.awt.Font("TH Sarabun New", 0, 18)); // NOI18N
+        jTable1.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+
+            },
+            new String [] {
+                "ลับดับ", "ชื่อสถานที่", "จำนวนคิวที่จอง"
+            }
+        ));
+        jTable1.setRequestFocusEnabled(false);
+        jTable1.setRowHeight(40);
+        jScrollPane1.setViewportView(jTable1);
+
+        loginAs.setFont(new java.awt.Font("TH Sarabun New", 0, 24)); // NOI18N
+        loginAs.setText("Login as:");
+
+        username.setFont(new java.awt.Font("TH Sarabun New", 0, 24)); // NOI18N
+        username.setText("<...username...>");
+
+        placeNo.setFont(new java.awt.Font("TH Sarabun New", 0, 24)); // NOI18N
+        placeNo.setText("Place: ");
+
+        number.setFont(new java.awt.Font("TH Sarabun New", 0, 24)); // NOI18N
+        number.setText("00");
+
+        dateText.setFont(new java.awt.Font("TH Sarabun New", 0, 24)); // NOI18N
+        dateText.setText("Date:");
+
+        date.setFont(new java.awt.Font("TH Sarabun New", 0, 24)); // NOI18N
+        date.setText("00/00/00  00:00");
+
+        back.setFont(new java.awt.Font("TH Sarabun New", 0, 24)); // NOI18N
+        back.setText("< ย้อนกลับ");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 1280, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(37, 37, 37)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(back, javax.swing.GroupLayout.PREFERRED_SIZE, 147, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(0, 0, Short.MAX_VALUE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 726, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addContainerGap(37, Short.MAX_VALUE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(loginAs, javax.swing.GroupLayout.PREFERRED_SIZE, 72, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(username, javax.swing.GroupLayout.PREFERRED_SIZE, 308, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(placeNo)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(number, javax.swing.GroupLayout.PREFERRED_SIZE, 29, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(dateText)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(date, javax.swing.GroupLayout.PREFERRED_SIZE, 123, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(58, 58, 58))))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 720, Short.MAX_VALUE)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addGap(27, 27, 27)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                        .addComponent(dateText, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(date, javax.swing.GroupLayout.PREFERRED_SIZE, 31, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(loginAs, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(username, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                        .addComponent(placeNo, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(number, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addComponent(back, javax.swing.GroupLayout.PREFERRED_SIZE, 37, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(49, 49, 49))
         );
 
         pack();
@@ -70,6 +153,8 @@ public class PlaceList extends javax.swing.JFrame {
         }
         //</editor-fold>
         //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
@@ -80,5 +165,14 @@ public class PlaceList extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JToggleButton back;
+    private javax.swing.JLabel date;
+    private javax.swing.JLabel dateText;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JTable jTable1;
+    private javax.swing.JLabel loginAs;
+    private javax.swing.JLabel number;
+    private javax.swing.JLabel placeNo;
+    private javax.swing.JLabel username;
     // End of variables declaration//GEN-END:variables
 }
